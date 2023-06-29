eb2e0ece7a231e5c6d6b3eec54bfd9301876fbc7
AliYagmurGames <aliyagmur.erad@gmail.com>
2023-06-29 17:36:21 +0300

    29_2_ali
    Some fixes and outline for basic enemy health.

---
+++ TheWitchOfShunrald/Assets/4. Scripts/CihanAkpınarMission/BasicEnemy/BasicSpawnedEnemyHealthBar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicSpawnedEnemyHealthBar : MonoBehaviour
+{
+    // update içerisinde rotasyonun sabit durmasýný saðlamanýn bir yolunu bulman lazým
+
+    // Ýçerisindeki health bar objesinin altýndaki sprite renderer ýn scale.x deðerini 1 ile 0 arasýnda deðiþtirerek can göstergesini oluþturabilirsin. 1 tam can 0 hiç can.
+
+}
