6778b953c960742105b4a8a9c35c6f84cc119df2
sid <sidaraygaz@hotmail.com>
2023-07-17 21:37:47 +0300

    17_5_sid
    cokbuyuk degisiklikler

--- TheWitchOfShunrald/Assets/4. Scripts/Sidar/NecroAI.cs
+++ TheWitchOfShunrald/Assets/4. Scripts/Sidar/NecroAI.cs
@@ -214,6 +214,8 @@ namespace Sidar
                 {
                     StopAllCoroutines();
                     animator.Play("BossIdle");
+                    agent.SetDestination(transform.position);
+
                 }
             }
         }
